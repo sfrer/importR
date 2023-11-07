59a1e09dc7cd4888927f97275ea7c14bd0f93a79
Simin Liu <sliu02@wesleyan.edu>
2023-11-07 15:05:42 -0500

    first commit

---
+++ R/import.R
@@ -0,0 +1,93 @@
+#' @title Import a dataset
+#'
+#' @description
+#' This \code{import} function can import data from
+#' delimited text files, simple JSON files, EXCEL spreadsheets,
+#' and statistical packages such as SAS, SPSS, and Stata.
+#' This wonderful.
+#'
+#' @details
+#' The import function is wrapper for the
+#' \href{https://haven.tidyverse.org}{haven},
+#' \href{https://readxl.tidyverse.org/}{readxl},
+#' \href{https://cran.r-project.org/web/packages/rjson/index.html}{rjson}
+#' and \href{https://github.com/r-lib/vroom}{vroom} packages.
+#'
+#' @note
+#' Complex nested JSON files will not be imported properly.
+#'
+#' @seealso
+#' \link[haven]{read_sas},
+#' \link[haven]{read_dta},
+#' \link[haven]{read_spss},
+#' \link[readxl]{read_excel},
+#' \link[rjson]{fromJSON},
+#' \link[vroom]{vroom}
+#'
+#' @param file datafile to import.
+#' @param ... parameters passed to the import function.
+#'
+#' @import haven
+#' @import readxl
+#' @import vroom
+#' @import tools
+#' @import rjson
+#' @export
+#' @return a data frame
+#'
+#' @examples
+#' \dontrun{
+#' # import a comma delimited file
+#' mydataframe <- import("mydata.csv")
+#'
+#' # import a SAS binary file
+#' mydataframe <- import("mydata.sas7bdat")
+#'
+#' # import the second worksheet of an Excel workbook
+#' mydataframe <- import("mydata.xlsx", sheet = 2)
+#'
+#' # prompt for a file to import
+#' mydataframe <- import()
+#' }
+
+
+import <- function(file, ...){
+
+  if (missing(file)){
+    file <- file.choose()
+  }
+
+  if (!file.exists(file)){
+    stop("File doesn't exist! Please choose another file")
+  }
+
+  extension <- tools::file_ext(file)
+  extension <- tolower(extension)
+
+  if(extension == "xls" | extension == "xlsx"){
+    dataset <- readxl::read_excel(file, ...)
+  }
+
+  else if(extension == "sas7bdat") {
+    dataset <- haven::read_sas(file, ...)
+  }
+
+  else if(extension =="sav") {
+    dataset <- haven::read_sav(file, ...)
+  }
+
+  else if(extension == "dta") {
+    dataset <- haven::read_dta(file, ...)
+  }
+
+  else if(extension == "json"){
+    dataset <- rjson::fromJSON(file, ...)
+    dataset <- as.data.frame(dataset)
+  }
+
+  else {
+    dataset<- vroom::vroom(file, ...)
+  }
+
+  return(dataset)
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# importR
+
+<img src="tools.jpg" width = "200" />
+
+The goal of importR is to create an R data frame from external data in SAS, SPSS, or Stata binary files, Excel Workbooks, or delimited text files.
+
+## Installation
+
+From CRAN
+
+``` r
+install.packages("importR")
+```
+
+For the development version
+
+``` r
+if (!require("remotes")){
+  install.packages("remotes")
+}
+remotes::install_github("rkabacoff/importR")
+```
